f739f93be4ea64e35ffd44e8d191de6251999cab
Lucas Mallmann Eich <89753549+LucasMallmannEich@users.noreply.github.com>
2021-10-19 21:01:17 -0300

    Update lancamento_de_dados.py

--- Lançamento de Dados/lancamento_de_dados.py
+++ Lançamento de Dados/lancamento_de_dados.py
@@ -1,4 +1,4 @@
-# 32 - Faça um programa que simula o lançamento de dois dados, d1 e d2, n vezes.
+# Faça um programa que simula o lançamento de dois dados, d1 e d2, n vezes.
 # E tem como saída o número de cada dado em relação entre eles (>, <, =) de cada lançamento.
 
 import random
