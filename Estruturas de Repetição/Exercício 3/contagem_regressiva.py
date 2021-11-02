b51ede14cb036506ef43b63ad0657e2bf960d15d
Lucas Mallmann Eich <89753549+LucasMallmannEich@users.noreply.github.com>
2021-11-02 19:11:11 -0300

    Add files via upload

---
+++ Estruturas de Repetição/Exercício 3/contagem_regressiva.py
@@ -0,0 +1,8 @@
+# 3 - Faça um algoritmo utilizando o comando while que mostra uma contagem regressiva na tela.
+# Iniciando em 10 e acabando em 0. Mostrar uma mensagem "FIM!" após a contagem.
+
+num = 10
+while num > -1:
+    print(num)
+    num = num - 1
+print('FIM!\n')
