f614ae45d88cbe258f4826571d4fd532b4544afb
Lucas Mallmann Eich <89753549+LucasMallmannEich@users.noreply.github.com>
2021-11-02 19:21:48 -0300

    Add files via upload

---
+++ Estruturas de Repetição/Exercício 4/utilizando_ranges.py
@@ -0,0 +1,5 @@
+# 4 - Escreva um programa que declare um inteiro, inicialize-o com 0, e incremente-o de 1000 em 1000.
+# Imprimindo seu valor na tela, até que seu valor seja 100000(cem mil).
+
+for num in range(0, 100_001, 1_000):
+    print(num)
