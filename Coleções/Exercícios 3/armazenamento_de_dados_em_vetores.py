ccfe362affe02ff83a60ad7e8d12975bbd1e7315
Lucas Mallmann Eich <89753549+LucasMallmannEich@users.noreply.github.com>
2021-11-04 06:42:38 -0300

    Add files via upload

---
+++ Coleções/Exercícios 3/armazenamento_de_dados_em_vetores.py
@@ -0,0 +1,18 @@
+# 3 - Ler um conjunto de números reais, armazenando-o em vetor e calcular o quadrado dos componentes deste vetor.
+# Assim, armazenando o resultado em outro vetor. Os conjuntos têm 10 elementos cada. Imprimir todos os conjuntos.
+
+counter = 0
+conjunto1 = []
+conjunto2 = []
+
+while counter != 10:
+    conjunto = float(input('Digite um conjunto de números reais: '))
+    conjunto1.append(conjunto)
+    counter += 1
+
+for x in conjunto1:
+    x1 = x*x
+    conjunto2.append(x1)
+
+print(conjunto1)
+print(conjunto2)
