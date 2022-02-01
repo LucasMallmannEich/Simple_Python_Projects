28a6d02b5dede5c8f2c4759cfcd182480803a103
Lucas Mallmann Eich <89753549+LucasMallmannEich@users.noreply.github.com>
2022-02-01 20:24:18 -0300

    Update maior_entre_dois_numeros.py

--- Estruturas Condicionais/Desafio 7/maior_entre_dois_numeros.py
+++ Estruturas Condicionais/Desafio 7/maior_entre_dois_numeros.py
@@ -2,12 +2,7 @@
 # Se por acaso, os dois números forem iguais, imprima a mensagem "Números iguais".
 
 numero1 = float(input('Digite um número:  '))
-if type(numero1) != float:
-    raise TypeError("Você deveria ter digitado um número.")
-
 numero2 = float(input('Digite mais um número:  '))
-if type(numero2) != float:
-    raise TypeError("Você deveria ter digitado um número.")
 
 if numero1 > numero2:
     print(f' O maior número entre os dois é o {numero1}.\n')
