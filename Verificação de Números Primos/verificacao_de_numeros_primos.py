72120ec7ff4427529bd48a4974961f9b087c5b78
Lucas Mallmann Eich <89753549+LucasMallmannEich@users.noreply.github.com>
2022-02-21 15:30:37 -0300

    Add files via upload

--- Verificação de Números Primos/verificacao_de_numeros_primos.py
+++ Verificação de Números Primos/verificacao_de_numeros_primos.py
@@ -1,11 +1,25 @@
-numero = int(input('Digite um número inteiro: '))
+def primo(num):
+    soma = 0
+    for n in range(2, num):
+        if num % n == 0:
+            soma = soma + 1
+    if soma == 0:
+        return f'O número {num} é primo.'
+    else:
+        return f'O número {num} não é primo.'
 
-soma = 0
 
-for n in range(2, numero):
-    if numero % n == 0:
-        soma += 1
-if soma != 0:
-    print(f' Esse número não é primo.')
-else:
-    print(f' Esse número é primo.')
+numero = 1
+
+while numero != 0:
+    try:
+        numero = int(input('Digite um número inteiro e positivo (caso queira sair, digite 0): '))
+    except ValueError:
+        print('Por favor, digite novamente.')
+    else:
+        if numero == 0:
+            print('Você saiu do programa.')
+        elif numero > 0:
+            print(primo(numero))
+        else:
+            print('Por favor, digite novamente.')
