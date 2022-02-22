4eea0e3bebdfec838b43f734238942f9964ee079
Lucas Mallmann Eich <89753549+LucasMallmannEich@users.noreply.github.com>
2022-02-22 01:25:08 -0300

    Add files via upload

---
+++ Orientação a Objetos/Desafio 5/poo_circulo.py
@@ -0,0 +1,41 @@
+"""
+Escreva um código que apresente a classe "Circulo", com atributos "raio", "area" e "perimetro". A classe ainda deve ter
+os métodos "calcular_area", "calcular_perimetro" e "imprimir". Os métodos "calcular_area" e "calcular_perimetro" devem
+efetuar seus respectivos cálculos e colocar os valores nos atributos "area" e "perimetro". O método "imprimir" deve
+mostrar na tela os valores de todos os atributos. Salienta-se que a área de um círculo é obtida pela fórmula
+(pi * raio * raio) e o perímetro por (2 * pi * raio).
+"""
+
+# Importação da biblioteca "math", com o intuito de utilizar o valor de pi.
+import math
+
+
+# Classe "Circurlo".
+class Circulo:
+
+    # Método construtor.
+    def __init__(self, raio):
+        if type(raio) == float or type(raio) == int and raio >= 0:
+            self.__raio = raio
+        else:
+            print('Você deveria ter digitado um valor numérico não negativo.')
+        self.__area = Circulo.calcular_area(self)
+        self.__perimetro = Circulo.calcular_perimetro(self)
+
+    # Método "calcular_area".
+    def calcular_area(self):
+        return math.pi * math.pi * self.__raio
+
+    # Método "calcular_perimetro".
+    def calcular_perimetro(self):
+        return math.pi * (self.__raio * 2)
+
+    # Método "imprimir".
+    def imprimir(self):
+        print(f' Raio: {self.__raio:.2f} m \t Área: {self.__area:.2f} m² \t Perímetro: {self.__perimetro:.2f} m')
+
+
+# Testando.
+circulo1 = Circulo(4.5)
+
+circulo1.imprimir()  # OUTPUT: Raio: 4.50 m 	 Área: 44.41 m² 	 Perímetro: 28.27 m
