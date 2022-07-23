4b600ab796cfde8004bb28bb588fa2032ddb7f16
Lucas Mallmann Eich <89753549+LucasMallmannEich@users.noreply.github.com>
2022-07-22 21:48:13 -0300

    Update numeros_reais.py

--- Coleções/Exercício(11)/numeros_reais.py
+++ Coleções/Exercício(11)/numeros_reais.py
@@ -1,6 +1,6 @@
 """
 Faça um programa que preencha uma lista com 10 números reais.
-Calcule e mostre a quantidade de números negativos e a soma dos números positivos desse vetor.
+Calcule e mostre a quantidade de números negativos e a soma dos números positivos dessa lista.
 """
 
 # Declaração da lista, do contador de ocorrências de números negativos e do somador de números positivos.
