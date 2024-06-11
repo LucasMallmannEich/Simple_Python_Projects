e5435a8bed6605625cace89811625ab91b8f8891
Lucas Mallmann Eich <89753549+LucasMallmannEich@users.noreply.github.com>
2024-06-11 20:07:26 -0300

    Add files via upload

---
+++ Estruturas de Repetição/Exercício(10)/first_50_even_numbers.py
@@ -0,0 +1,4 @@
+"""
+10 - Write a program that calculates and shows the sum of the first 50 even numbers.
+"""
+print(f' The sum of the first 50 even numbers is equal to {sum(range(0, 100, 2))}.\n')
