a881f1a106df9490a98e28cdb795fc8eec8e62c3
Lucas Mallmann Eich <89753549+LucasMallmannEich@users.noreply.github.com>
2024-06-14 21:01:39 -0300

    Add files via upload

---
+++ Estruturas de Repetição/Exercício(11)/n_integer_numbers.py
@@ -0,0 +1,18 @@
+"""
+11 - Write a program that reads an integer positive number N and displays all the natural numbers from 0 to N in
+ascending order.
+"""
+N = ''
+# Reading the number.
+while type(N) is not int:
+    try:
+        N = int(input('Write an integer positive number: '))
+    except ValueError:
+        print(' You must insert an integer positive number.\n')
+    else:
+        while N <= 0:
+            print(' Please, insert an integer positive number.\n')
+            N = int(input('Write an integer positive number: '))
+# Displaying all the natural numbers from 0 to N in ascending order.
+for num in range(0, N+1, 1):
+    print(num)
